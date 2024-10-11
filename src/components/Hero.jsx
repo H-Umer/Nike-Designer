6c8313cd8788f43449bfd15b1d12cc003f2a8aea
Hafiz Umer <umersayam96@gmail.com>
2024-10-11 18:23:11 +0500

    Nike

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <main className='hero container'>
+          <div className='hero-content'>
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND   WE’RE HERE TO HELP YOU WITH OUR SHOES.
+          </p>
+          
+          
+          <div className='hero-btn'>
+                  <button>Shop Now</button>
+                  <button className='secondary-btn'>Category</button>
+            </div>
+           
+            <div className='shopping'>
+                 <p>Also Available On</p>
+            </div>
+
+                   <div className='brand-icons'>
+                   <img src="/Images/amazon.png" alt="amazon-logo" />
+                   <img src="/Images/flipkart.png" alt="flipkart-logo" />
+                   </div>
+                   
+          </div>
+          <div className='hero-image'> 
+           <img src="/Images/hero-image.png" alt="hero-image" style={{height: 390}} />
+          </div>
+      </main>
+    </div>
+  )
+}
+
+export default HeroSection
